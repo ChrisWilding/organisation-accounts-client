3634770209f286c24c05c94db4b9c52f041082dd
Chris Wilding <hello@chriswilding.co.uk>
2022-03-20 14:47:06 +0000

    Add Accounts.Create (#6)
    * Add Accounts.Create
    
    * Bring up docker stack for tests

--- client.go
+++ client.go
@@ -1,17 +1,49 @@
 package oac
 
-import "net/http"
+import (
+	"net/http"
+)
 
+const BASE_URL = "https://api.form3.tech"
+
+// Client is the root of the API.
 type Client struct {
 	HTTPClient *http.Client
-	Accounts   Accounts
+	Accounts   *Accounts
+	baseURL    string
 }
 
-func NewClient(httpClient *http.Client) Client {
-	return Client{
-		HTTPClient: httpClient,
-		Accounts: Accounts{
-			HTTPClient: httpClient,
-		},
+// ClientOption is used to customise the Client.
+type ClientOption func(c *Client)
+
+// WithBaseURL returns a ClientOption that changes the API base URL used by the Client.
+// For example, this can be used to override the base URL for tests or to use a non-production
+// environment.
+func WithBaseURL(baseURL string) ClientOption {
+	return func(c *Client) {
+		c.baseURL = baseURL
 	}
+}
+
+// NewClient initialised a Client, wrapping an implementation of http.Client and a slice
+// of ClientOptions that are used to customise the Client.
+func NewClient(httpClient *http.Client, opts ...ClientOption) Client {
+	c := Client{
+		HTTPClient: httpClient,
+	}
+	c.Accounts = &Accounts{
+		c: &c,
+	}
+	c.baseURL = BASE_URL
+
+	for _, opt := range opts {
+		opt(&c)
+	}
+
+	return c
+}
+
+// String is a utility function to convert a string value to a pointer.
+func String(s string) *string {
+	return &s
 }
