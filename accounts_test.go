afd4d6d8ef766c09e0a0458459b9f5665dc9b1df
Chris Wilding <hello@chriswilding.co.uk>
2022-03-20 16:15:48 +0000

    Add Accounts.Fetch (#7)

--- accounts_test.go
+++ accounts_test.go
@@ -9,27 +9,46 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var name = uuid.New().String()
+var attrs = AccountAttributes{
+	BankID:       "400300",
+	BankIDCode:   "GBDSC",
+	BaseCurrency: "GBP",
+	Bic:          "NWBKGB22",
+	Country:      String("GB"),
+	Name:         []string{name},
+}
+
+var acc = AccountData{
+	Attributes:     &attrs,
+	ID:             uuid.New().String(),
+	OrganisationID: uuid.New().String(),
+	Type:           "accounts",
+}
+
 func TestCreateAccount(t *testing.T) {
-	name := uuid.New().String()
-	attrs := AccountAttributes{
-		BankID:       "400300",
-		BankIDCode:   "GBDSC",
-		BaseCurrency: "GBP",
-		Bic:          "NWBKGB22",
-		Country:      String("GB"),
-		Name:         []string{name},
-	}
-
-	acc := AccountData{
-		Attributes:     &attrs,
-		ID:             uuid.New().String(),
-		OrganisationID: uuid.New().String(),
-		Type:           "accounts",
-	}
-
 	c := NewClient(http.DefaultClient, WithBaseURL("http://localhost:8080"))
 
 	res, err := c.Accounts.Create(context.Background(), &acc)
+
+	assert.Equal(t, attrs.BankID, res.Attributes.BankID)
+	assert.Equal(t, attrs.BankIDCode, res.Attributes.BankIDCode)
+	assert.Equal(t, attrs.BaseCurrency, res.Attributes.BaseCurrency)
+	assert.Equal(t, attrs.Country, res.Attributes.Country)
+	assert.Len(t, attrs.Name, 1)
+	assert.Equal(t, attrs.Name[0], res.Attributes.Name[0])
+	assert.Equal(t, acc.ID, res.ID)
+	assert.Equal(t, acc.OrganisationID, res.OrganisationID)
+	assert.Equal(t, acc.Type, "accounts")
+	assert.Equal(t, int64(0), *res.Version)
+
+	assert.Nil(t, err)
+}
+
+func TestFetchAccount(t *testing.T) {
+	c := NewClient(http.DefaultClient, WithBaseURL("http://localhost:8080"))
+
+	res, err := c.Accounts.Fetch(context.Background(), acc.ID)
 
 	assert.Equal(t, attrs.BankID, res.Attributes.BankID)
 	assert.Equal(t, attrs.BankIDCode, res.Attributes.BankIDCode)
