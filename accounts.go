e95553f3f7310ee9a0fe609beabb3a9ec344b99f
Chris Wilding <hello@chriswilding.co.uk>
2022-03-20 14:37:44 +0000

    Add Accounts.Create

--- accounts.go
+++ accounts.go
@@ -1,22 +1,62 @@
 package oac
 
-import "net/http"
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"net/http"
+)
 
+// Accounts groups functionality to create, delete and fetch AccountData.
 type Accounts struct {
-	HTTPClient *http.Client //nolint
+	c *Client
 }
 
-type Account struct {
+type accountReq struct {
+	Data *AccountData `json:"data,omitempty"`
 }
 
-func (a *Accounts) Create(acount Account) (*Account, error) {
-	return nil, nil
+type accountRes struct {
+	Data *AccountData `json:"data,omitempty"`
 }
 
-func (a *Accounts) Delete() (bool, error) {
-	return true, nil
+// Create a new account using the provided AccountData.
+func (a *Accounts) Create(ctx context.Context, accountData *AccountData) (*AccountData, error) {
+	var ar = accountReq{
+		Data: accountData,
+	}
+	var buf bytes.Buffer
+	err := json.NewEncoder(&buf).Encode(ar)
+	if err != nil {
+		return nil, err
+	}
+
+	url := a.c.baseURL + "/v1/organisation/accounts"
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, &buf)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Accept", "application/vnd.api+json")
+
+	res, err := a.c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	var out accountRes
+	err = json.NewDecoder(res.Body).Decode(&out)
+	if err != nil {
+		return nil, err
+	}
+
+	return out.Data, nil
 }
 
-func (a *Accounts) Fetch() (*Account, error) {
-	return nil, nil
-}
+// func (a *Accounts) Delete() (bool, error) {
+// 	return true, nil
+// }
+
+// func (a *Accounts) Fetch() (*Account, error) {
+// 	return nil, nil
+// }
--- client_test.go
+++ client_test.go
@@ -11,3 +11,9 @@ func TestNewClient(t *testing.T) {
 	c := NewClient(http.DefaultClient)
 	assert.NotNil(t, c)
 }
+
+func TestOverrideClientBaseURL(t *testing.T) {
+	expected := "https://www.example.com"
+	c := NewClient(http.DefaultClient, WithBaseURL(expected))
+	assert.Equal(t, expected, c.baseURL)
+}
